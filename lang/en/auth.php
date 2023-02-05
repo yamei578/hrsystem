b6387dc93564399890b3a58847147446ff94e7ca
yamei578 <yamei_lama@hotmail.com>
2023-02-05 17:31:19 -0500

    mensaje en inicio de sesión

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
