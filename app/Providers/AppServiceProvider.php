0dc396d15c70d3ec005c0296ce373e0b7600684b
yamei578 <yamei_lama@hotmail.com>
2023-02-20 03:04:20 -0500

    reverso

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if($this->app->environment('production')) {
-            \URL::forceScheme('https');
-        }
 
     }
 }
