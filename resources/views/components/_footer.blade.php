faeff12a6a4f35227f8dee62a0ab344474696abe
yamei578 <yamei_lama@hotmail.com>
2023-02-26 12:38:22 -0500

    quitar footer

--- resources/views/components/_footer.blade.php
+++ resources/views/components/_footer.blade.php
@@ -1,7 +1,7 @@
-<footer class="sticky-footer bg-white">
+<!--<footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Yamei Lama - Andrés Álvarez</span>
+           
           </div>
         </div>
-      </footer>
+      </footer>-->
