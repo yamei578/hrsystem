d1822a15cbc0e180e357b32d9689a261674a6eb8
yamei578 <yamei_lama@hotmail.com>
2023-02-26 12:55:27 -0500

    footer

--- resources/views/components/_footer.blade.php
+++ resources/views/components/_footer.blade.php
@@ -1,7 +1,7 @@
-<!--<footer class="sticky-footer bg-white">
+<footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-           
+              Sistema de Gestión de Recursos Humanos para Empresa XYZ.
           </div>
         </div>
-      </footer>-->
+      </footer>
