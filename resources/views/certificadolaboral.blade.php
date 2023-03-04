cea6ffaa1e276ba9c8ac4191b82dcd660925e21b
yamei578 <yamei_lama@hotmail.com>
2023-03-04 11:52:22 -0500

    formato certificado

--- resources/views/certificadolaboral.blade.php
+++ resources/views/certificadolaboral.blade.php
@@ -29,7 +29,7 @@ body{
         <h1 style="text-align:center; margin-top:100px; margin-bottom:50px;">C E R T I F I C A D O</h1>      
        <p>Certifico por medio de la presente que <b>{{$nombreColaborador}}</b> con cédula de identidad <b>No. {{$cedulaColaborador}}</b> labora bajo la 
         dependencia de la compañía <b>EMPRESA XYZ S.A.</b> desde el {{$fechaLaboralCompleta}} hasta la actualidad en el cargo de <b>{{$trabajoColaborador}}</b>
-        percibiendo una remuneración mensual de ${{$salario}}.</p>
+        percibiendo una remuneración mensual de ${{number_format($salario,2)}}.</p>
 
         <p>El interesado puede hacer uso de esta certificación para los fines que estime conveniente.</p>
 </div>
